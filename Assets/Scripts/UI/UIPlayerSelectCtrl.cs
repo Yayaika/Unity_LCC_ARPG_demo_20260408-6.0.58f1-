591cd1213d4091ab5d5e6f31287990af1d010505
Yayaika <126582582+Yayaika@users.noreply.github.com>
2026-05-22 23:07:50 +0800

    Update_class07_添加角色圖標和選角脚本改進
    更新UIPlayerSelectCtrl脚本結構化同時控制鏡頭和圖標

--- Assets/Scripts/UI/UIPlayerSelectCtrl.cs
+++ Assets/Scripts/UI/UIPlayerSelectCtrl.cs
@@ -1,5 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
 using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class UIPlayerSelectCtrl : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class UIPlayerSelectCtrl : MonoBehaviour
     /// 索引原始值
     /// </summary>
     private int _index;
+    /// <summary>
+    /// 索引操作變數
+    /// </summary>
     private int index
     {
         get
@@ -15,30 +20,65 @@ public class UIPlayerSelectCtrl : MonoBehaviour
         }
         set
         {// 小於0特殊處理成尾號，否則取餘數
-            _index = value < 0 ? vCams.Length - 1 : value % vCams.Length;
+            _index = value < 0 ? charOptions.Length - 1 : value % charOptions.Length;
+        }
+    }
+
+    /// <summary>
+    /// 角色選項資料結構
+    /// </summary>
+    [Serializable]
+    public struct CharOption
+    {
+        /// <summary>
+        /// 虛擬鏡頭設定
+        /// </summary>
+        public CinemachineCamera vCam;
+        /// <summary>
+        /// UI狀態提示(是否選中)
+        /// </summary>
+        public Toggle toggle;
+        /// <summary>
+        /// 角色選項開關切換
+        /// </summary>
+        /// <param name="B">開/關</param>
+        public void Switch(bool B)
+        {
+            vCam.Priority.Enabled = B;
+            toggle.isOn = B;
         }
     }
     /// <summary>
-    /// 虛擬鏡頭設定集合物
+    /// [陣列] 虛擬鏡頭設定集合物
     /// </summary>
-    public CinemachineCamera[] vCams;
+    public CharOption[] charOptions;
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    private void Start()
+    {
+        // 選中預設第一位角色
+        charOptions[index].Switch(true);
+    }
 
     /// <summary>
     /// 下一個角色(鏡頭逆轉)
     /// </summary>
     public void NextPlayer()
     {
-        vCams[index].Priority.Enabled = false;
-        index++;// 索引增加
-        vCams[index].Priority.Enabled = true;
+        charOptions[index].Switch(false);
+        index++;//索引增加
+        charOptions[index].Switch(true);
+
     }
     /// <summary>
     /// 上一個角色(鏡頭順轉)
     /// </summary>
     public void PrePlayer()
     {
-        vCams[index].Priority.Enabled = false;
+        charOptions[index].Switch(false);
         index--;// 索引減少
-        vCams[index].Priority.Enabled = true;
+        charOptions[index].Switch(true);
     }
 }
