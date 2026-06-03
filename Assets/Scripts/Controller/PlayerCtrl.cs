da06a83473acd918fc4ddcd3b0b8464fcd0f0123
Yayaika <126582582+Yayaika@users.noreply.github.com>
2026-06-04 01:30:30 +0800

    Update_角色產生器和控制輸入脚本

--- Assets/Scripts/Controller/PlayerCtrl.cs
+++ Assets/Scripts/Controller/PlayerCtrl.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCtrl : MonoBehaviour
 {
@@ -13,8 +14,50 @@ public class PlayerCtrl : MonoBehaviour
     private CharacterController charCtrl => _charCtrl ??= GetComponent<CharacterController>();
     #endregion 基础元件
 
+    #region 基礎参数
+    private Controls _controls;
+    private Vector3 _facingVector;
+    [SerializeField]
+    private float _moveSpeed = 2f;
+    #endregion 基礎参数
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    #region 公用参数
+    /// <summary>
+    /// 產生一組預設好的控制檔
+    /// </summary>
+
+    public Controls InputCtrl => _controls ??= new Controls();
+    /// <summary>
+    /// 從輸入取得的方向向量
+    /// </summary>
+    public Vector2 MoveInput => InputCtrl.Play.Move.ReadValue<Vector2>();
+    public Vector3 FacingVector
+    {
+        get
+        {
+            _facingVector.x = MoveInput.x;
+            _facingVector.z = MoveInput.y;
+            return _facingVector;
+        }
+    }
+    /// <summary>
+    /// 依據方向向量判斷是否正在移動
+    /// </summary>
+    public bool IsMoving => MoveInput != Vector2.zero;
+    #endregion 公用参数
+
+    #region 生命周期
+    private void OnEnable()
+    {
+        InputCtrl.Play.Enable();
+         
+    }
+
+    private void OnDisable()
+    {
+        InputCtrl.Play.Disable();
+    }
+
     void Start()
     {
         
@@ -23,6 +66,21 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+    #endregion 生命周期
+
+    void Move()
+    {
+        if (!IsMoving) return;
+        //轉向
+        charCtrl.transform.rotation = Quaternion.LookRotation(FacingVector);
+        //前進
+        charCtrl.Move(transform.forward * _moveSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+
     }
 }
---
+++ Assets/Scripts/Controller/SpawnCtrl.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SpawnCtrl : MonoBehaviour
+{
+
+    #region 基础元件
+    [SerializeField]
+    private PlayerDB _playerDB;
+
+    #endregion 基础元件
+
+    #region 公用参数
+    /// <summary>
+    /// 玩家的索引（編號）
+    /// </summary>
+    private int PlayerIndex => GameManager.playerIndex;
+    private PlayerCtrl CurrentPlayer => _playerDB.GetPlayerData(PlayerIndex).playerCtrl;
+    #endregion 公用参数
+
+    /// <summary>
+    /// 誕生控制初始
+    /// </summary>
+    void Start()
+    {
+        Spawn();
+    }
+
+    /// <summary>
+    /// 產生對應選取的角色
+    /// </summary>
+    private void Spawn()
+    {
+        Instantiate(CurrentPlayer, transform.position, transform.rotation);
+    }
+}
